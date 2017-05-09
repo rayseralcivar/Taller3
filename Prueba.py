910c6d3abd9266f88597f864276b9aef1a1a6d25
rayseralcivar <rbvb1998@gmail.com>
2017-05-09 08:10:08 -0500

    Arreglo

--- Prueba.py
+++ Prueba.py
@@ -1,13 +1,21 @@
 def mayor(lista):
-    i = 0
     for m in lista:
         l = m
     print(m)
 
 
+def cambiar(lista, a, b):
+    print("lista original:", lista)
+    for i in lista:
+        if i < a:
+            l = lista.index(i)
+            lista.remove(i)
+            lista.insert(l, b)
+    print("nueva lista:", lista)
 
 
-
-
-
-lista = [10,5,11,2]
+a = 0
+b = 0
+lista = [1, 2, 3, -2, 4, 7, 3, -4, 3]
+cambiar(lista, a, b)
+mayor(lista)
